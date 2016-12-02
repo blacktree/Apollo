b2aa2927c80398f4540cd43785506b35409d8725
Nepxion <1394997@qq.com>
2016-12-02 19:59:32 +0800

    Config repository

--- apollo-config-server/src/main/java/com/nepxion/apollo/config/ConfigServer2.java
+++ apollo-config-server/src/main/java/com/nepxion/apollo/config/ConfigServer2.java
@@ -12,12 +12,14 @@ package com.nepxion.apollo.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServer {
+@EnableDiscoveryClient
+public class ConfigServer2 {
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ConfigServer.class).web(true).run(args);
+        new SpringApplicationBuilder(ConfigServer2.class).web(true).run(args);
     }
 }
