aebd43406206736037828cb1cfddcfb0d08841d9
Nepxion <1394997@qq.com>
2016-12-05 20:13:48 +0800

    Add Zuul logic

---
+++ apollo-gateway-zuul/src/main/java/com/nepxion/apollo/gateway/ZuulGateway.java
@@ -0,0 +1,29 @@
+package com.nepxion.apollo.gateway;
+
+/**
+ * <p>Title: Nepxion Apollo</p>
+ * <p>Description: Nepxion Apollo For Spring Cloud</p>
+ * <p>Copyright: Copyright (c) 2016</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @email 1394997@qq.com
+ * @version 1.0
+ */
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGateway {
+    @Bean
+    public ZuulAccessFilter accessFilter() {
+        return new ZuulAccessFilter();
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ZuulGateway.class).web(true).run(args);
+    }
+}
