7bcc5ffa4f2478dbc473631677932646f0c2402e
Nepxion <1394997@qq.com>
2017-06-18 22:11:26 +0800

    修改SQL

--- apollo-oauth-server/src/main/resources/schema.sql
+++ apollo-oauth-server/src/main/resources/schema.sql
@@ -1,10 +1,11 @@
-CREATE SCHEMA IF NOT EXISTS `oauth` DEFAULT CHARACTER SET utf8 ;
-USE `oauth` ;
+/*CREATE DATABASE IF NOT EXISTS `authorization` CHARSET utf8 COLLATE utf8_general_ci;*/
 
--- -----------------------------------------------------
--- Table `oauth`.`oauth_client_details`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oauth`.`oauth_client_details` (
+-- ----------------------------
+--  Table structure for `oauth_client_details`
+-- ----------------------------
+DROP TABLE IF EXISTS `oauth_client_details`;
+CREATE TABLE `oauth_client_details`
+(
   `client_id` VARCHAR(128) NOT NULL,
   `resource_ids` VARCHAR(256) NULL DEFAULT NULL,
   `client_secret` VARCHAR(256) NULL DEFAULT NULL,
@@ -16,27 +17,35 @@ CREATE TABLE IF NOT EXISTS `oauth`.`oauth_client_details` (
   `refresh_token_validity` INT(11) NULL DEFAULT NULL,
   `additional_information` VARCHAR(4096) NULL DEFAULT NULL,
   `autoapprove` VARCHAR(256) NULL DEFAULT NULL,
-  PRIMARY KEY (`client_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  PRIMARY KEY (`client_id`)
+)
+ENGINE = innodb
+CHARSET = utf8
+COLLATE = utf8_general_ci;
 
--- -----------------------------------------------------
--- Table `oauth`.`oauth_client_token`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oauth`.`oauth_client_token` (
+-- ----------------------------
+--  Table structure for `oauth_client_token`
+-- ----------------------------
+DROP TABLE IF EXISTS `oauth_client_token`;
+CREATE TABLE `oauth_client_token`
+(
   `token_id` VARCHAR(256) NULL DEFAULT NULL,
   `token` BLOB NULL DEFAULT NULL,
   `authentication_id` VARCHAR(128) NOT NULL,
   `user_name` VARCHAR(256) NULL DEFAULT NULL,
   `client_id` VARCHAR(256) NULL DEFAULT NULL,
-  PRIMARY KEY (`authentication_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  PRIMARY KEY (`authentication_id`)
+)
+ENGINE = innodb
+CHARSET = utf8
+COLLATE = utf8_general_ci;
 
--- -----------------------------------------------------
--- Table `oauth`.`oauth_access_token`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oauth`.`oauth_access_token` (
+-- ----------------------------
+--  Table structure for `oauth_access_token`
+-- ----------------------------
+DROP TABLE IF EXISTS `oauth_access_token`;
+CREATE TABLE `oauth_access_token`
+(
   `token_id` VARCHAR(256) NULL DEFAULT NULL,
   `token` BLOB NULL DEFAULT NULL,
   `authentication_id` VARCHAR(128) NOT NULL,
@@ -44,46 +53,62 @@ CREATE TABLE IF NOT EXISTS `oauth`.`oauth_access_token` (
   `client_id` VARCHAR(256) NULL DEFAULT NULL,
   `authentication` BLOB NULL DEFAULT NULL,
   `refresh_token` VARCHAR(256) NULL DEFAULT NULL,
-  PRIMARY KEY (`authentication_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  PRIMARY KEY (`authentication_id`)
+)
+ENGINE = innodb
+CHARSET = utf8
+COLLATE = utf8_general_ci;
 
--- -----------------------------------------------------
--- Table `oauth`.`oauth_refresh_token`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oauth`.`oauth_refresh_token` (
+-- ----------------------------
+--  Table structure for `oauth_refresh_token`
+-- ----------------------------
+DROP TABLE IF EXISTS `oauth_refresh_token`;
+CREATE TABLE `oauth_refresh_token`
+(
   `token_id` VARCHAR(256) NULL DEFAULT NULL,
   `token` BLOB NULL DEFAULT NULL,
-  `authentication` BLOB NULL DEFAULT NULL)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  `authentication` BLOB NULL DEFAULT NULL
+)
+ENGINE = innodb
+CHARSET = utf8
+COLLATE = utf8_general_ci;
 
--- -----------------------------------------------------
--- Table `oauth`.`oauth_code`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oauth`.`oauth_code` (
+-- ----------------------------
+--  Table structure for `oauth_code`
+-- ----------------------------
+DROP TABLE IF EXISTS `oauth_code`;
+CREATE TABLE `oauth_code`
+(
   `code` VARCHAR(256) NULL DEFAULT NULL,
-  `authentication` BLOB NULL DEFAULT NULL)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  `authentication` BLOB NULL DEFAULT NULL
+)
+ENGINE = innodb
+CHARSET = utf8
+COLLATE = utf8_general_ci;
 
--- -----------------------------------------------------
--- Table `oauth`.`oauth_approvals`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oauth`.`oauth_approvals` (
+-- ----------------------------
+--  Table structure for `oauth_approvals`
+-- ----------------------------
+DROP TABLE IF EXISTS `oauth_approvals`;
+CREATE TABLE `oauth_approvals`
+(
   `userId` VARCHAR(256) NULL DEFAULT NULL,
   `clientId` VARCHAR(256) NULL DEFAULT NULL,
   `scope` VARCHAR(256) NULL DEFAULT NULL,
   `status` VARCHAR(10) NULL DEFAULT NULL,
   `expiresAt` DATETIME NULL DEFAULT NULL,
-  `lastModifiedAt` DATETIME NULL DEFAULT NULL)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  `lastModifiedAt` DATETIME NULL DEFAULT NULL
+)
+ENGINE = innodb
+CHARSET = utf8
+COLLATE = utf8_general_ci;
 
--- -----------------------------------------------------
--- Table `oauth`.`clientdetails`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `oauth`.`clientdetails` (
+-- ----------------------------
+--  Table structure for `clientdetails`
+-- ----------------------------
+DROP TABLE IF EXISTS `clientdetails`;
+CREATE TABLE `clientdetails`
+(
   `appId` VARCHAR(128) NOT NULL,
   `resourceIds` VARCHAR(256) NULL DEFAULT NULL,
   `appSecret` VARCHAR(256) NULL DEFAULT NULL,
@@ -95,9 +120,15 @@ CREATE TABLE IF NOT EXISTS `oauth`.`clientdetails` (
   `refresh_token_validity` INT(11) NULL DEFAULT NULL,
   `additionalInformation` VARCHAR(4096) NULL DEFAULT NULL,
   `autoApproveScopes` VARCHAR(256) NULL DEFAULT NULL,
-  PRIMARY KEY (`appId`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  PRIMARY KEY (`appId`)
+)
+ENGINE = innodb
+CHARSET = utf8
+COLLATE = utf8_general_ci;
 
-# client_id, resource_ids, client_secret, scope, authorized_grant_types, web_server_redirect_uri, authorities, access_token_validity, refresh_token_validity, additional_information, autoapprove
-# 'client', NULL, 'secret', 'app', 'authorization_code', 'http://www.baidu.com', NULL, NULL, NULL, NULL, NULL
+-- ----------------------------
+--  Records of `oauth_client_details`
+-- ----------------------------
+BEGIN;
+INSERT INTO `oauth_client_details` VALUES ('client', NULL, 'secret', 'app', 'authorization_code', 'http://www.baidu.com', NULL, NULL, NULL, NULL, NULL);
+COMMIT;
