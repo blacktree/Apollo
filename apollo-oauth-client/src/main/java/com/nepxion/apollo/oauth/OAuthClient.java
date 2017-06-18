3d55f4e686f929f20de17d771fd042669c9cb1c4
Nepxion <1394997@qq.com>
2017-06-18 23:24:29 +0800

    增加OAuth客户端

---
+++ apollo-oauth-client/src/main/java/com/nepxion/apollo/oauth/OAuthClient.java
@@ -0,0 +1,25 @@
+package com.nepxion.apollo.oauth;
+
+/**
+ * <p>Title: Nepxion Apollo</p>
+ * <p>Description: Nepxion Apollo For Spring Cloud</p>
+ * <p>Copyright: Copyright (c) 2016</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @email 1394997@qq.com
+ * @version 1.0
+ */
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableOAuth2Sso
+public class OAuthClient {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(OAuthClient.class).web(true).run(args);
+    }
+}
