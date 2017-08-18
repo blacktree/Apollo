29a23a3b4b3446b7687f8f83785fa5d28d85dc48
renhj <renhj@fenqi.im>
2017-08-18 17:09:32 +0800

    增加状态机

--- apollo-state-machine/src/main/java/com/nepxion/apollo/state/machine/StateFactory.java
+++ apollo-state-machine/src/main/java/com/nepxion/apollo/state/machine/StateFactory.java
@@ -14,11 +14,42 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.nepxion.apollo.state.machine.enums.Actions;
+import com.nepxion.apollo.state.machine.enums.Events;
 import com.nepxion.apollo.state.machine.enums.States;
 
 public class StateFactory {
-    public static List<Actions> getActions(States states) {
-        switch (states) {
+    // 根据当前状态和事件，驱动出下一个状态
+    public static States getState(States state, Events event) {
+        if (state == States.STATE_WAIT_AUDIT && event == Events.EVENT_AUDIT_PASS) {
+            return States.STATE_WAIT_SEND;
+        }
+
+        if (state == States.STATE_WAIT_AUDIT && event == Events.EVENT_AUDIT_REJECT) {
+            return States.STATE_AUDIT_REJECT;
+        }
+
+        if (state == States.STATE_WAIT_SEND && event == Events.EVENT_SEND) {
+            return States.STATE_SEND_COMPLETE;
+        }
+
+        if (state == States.STATE_WAIT_AUDIT && event == Events.EVENT_DELETE) {
+            return States.STATE_DELETE_COMPLETE;
+        }
+
+        if (state == States.STATE_WAIT_SEND && event == Events.EVENT_DELETE) {
+            return States.STATE_DELETE_COMPLETE;
+        }
+
+        if (state == States.STATE_AUDIT_REJECT && event == Events.EVENT_DELETE) {
+            return States.STATE_DELETE_COMPLETE;
+        }
+
+        throw new IllegalArgumentException("Invalid state=[" + state + "] with event=[" + event + "]");
+    }
+
+    // 根据当前状态获取下一步动作列表
+    public static List<Actions> getActions(States state) {
+        switch (state) {
             case STATE_WAIT_AUDIT:
                 return Arrays.asList(Actions.ACTION_TO_AUDIT, Actions.ACTION_TO_DELETE);
             case STATE_AUDIT_REJECT:
@@ -31,6 +62,6 @@ public class StateFactory {
                 return Arrays.asList();
         }
 
-        return null;
+        throw new IllegalArgumentException("Invalid state=[" + state + "]");
     }
 }
